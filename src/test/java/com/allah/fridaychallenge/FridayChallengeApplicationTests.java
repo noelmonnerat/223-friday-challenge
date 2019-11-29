cc00509e007698178ed5ec84050ce245fcfe9fc3
noelmonnerat <noel.j.monnerat@gmail.com>
2019-11-29 08:45:43 +0100

    first push

---
+++ src/test/java/com/allah/fridaychallenge/FridayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allah.fridaychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
