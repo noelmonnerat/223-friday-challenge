237a7cb7e3e19547d0575a8e10ab78e782a683df
Sina <sina.blattmann@nyp.ch>
2019-11-29 11:38:34 +0100

    added classes

--- src/main/java/com/allah/fridaychallenge/parkingspace/ParkingSpace.java
+++ src/main/java/com/allah/fridaychallenge/parkingspace/ParkingSpace.java
@@ -1,4 +1,18 @@
 package com.allah.fridaychallenge.parkingspace;
 
-public class ParkingSpace {
+
+import com.allah.fridaychallenge.generic.ExtendedEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "parking_space")
+public class ParkingSpace extends ExtendedEntity {
+
+  @Column(name = "parking_nr")
+  private int parkingNumber;
+
 }
